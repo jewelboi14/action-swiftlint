01f1db6e7d0af235425287e43d059733e7cf9d4c
Mikhael Yurov <myurov@mynalabs.ai>
2023-07-24 14:41:51 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# convert swiftlint's output into GitHub Actions Logging commands
-# https://help.github.com/en/github/automating-your-workflow-with-github-actions/development-tools-for-github-actions#logging-commands
-
 function stripPWD() {
-    if ! ${WORKING_DIRECTORY+false};
+    if [ -n "${WORKING_DIRECTORY}" ];
     then
-        cd - > /dev/null
+        cd "${WORKING_DIRECTORY}" || exit 1
     fi
     sed -E "s/$(pwd|sed 's/\//\\\//g')\///"
 }
@@ -15,23 +12,20 @@ function convertToGitHubActionsLoggingCommands() {
     sed -E 's/^(.*):([0-9]+):([0-9]+): (warning|error|[^:]+): (.*)/::\4 file=\1,line=\2,col=\3::\5/'
 }
 
-sh -c "git config --global --add safe.directory $PWD"
-
-if ! ${WORKING_DIRECTORY+false};
-then
-	cd ${WORKING_DIRECTORY}
-fi
+# Set safe.directory for git
+git config --global --add safe.directory "$PWD"
 
 # Fetch the base_ref from the remote and update the local branch
-git fetch --prune --no-tags --depth=1 origin +refs/heads/${{ github.base_ref }}:refs/heads/${{ github.base_ref }}
-git checkout ${{ github.base_ref }}
+git fetch --prune --no-tags --depth=1 origin "+refs/heads/${BASE_REF}:refs/heads/${BASE_REF}"
+git checkout "${BASE_REF}"
 
 # Compare the base_ref with the current branch (HEAD) to get changed files
-changedFiles=$(git --no-pager diff --name-only --relative ${{ github.base_ref }} HEAD)
+changedFiles=$(git --no-pager diff --name-only --relative "${BASE_REF}" HEAD)
 
 if [ -z "$changedFiles" ]; then
     echo "No files changed"
     exit
 fi
 
+# Run SwiftLint on changed files
 set -o pipefail && swiftlint "$@" -- $changedFiles | stripPWD | convertToGitHubActionsLoggingCommands
